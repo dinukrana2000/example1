b89c3e49e3fa6b2b3244803f2dfd64b6fb4467c6
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-08 19:39:22 +0530

    React_props(parent props/children props/multi props))

--- src/Component/Topic.js
+++ src/Component/Topic.js
@@ -3,17 +3,18 @@ import './topic.css'
 
 
 
-export default function Topic() {
+export default function Topic(props) {
 
   //const ex1="topic1";
-  const ex2="topic2";
+  //const ex2="topic2";
   //const array=[10,20,30,40,50,60]
   //const object1={kottu:"200",rice:"300"}  not support object types directly
  
   return (
     <div>
         <div className='topicbox'>
-            <span className='span'>{ex2}</span>
+            <span className='span'>My favourite vehicle is {props.name} price is {props.price}</span>
+            {props.children}
 
         </div>
       
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,8 +7,16 @@ export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
-      <Topic/>
-      <Content/>
+      <Topic name="civic" price="50 USD">
+        <span>This is span</span>
+        </Topic><br/>
+      <Topic name="maruti" price="20 USD">
+        <button>This is button</button>
+        </Topic><br/>
+      <Topic name="bmw" price="100 USD">
+        <p>this is para</p>
+        </Topic><br/>
+      
     </div>
   )
 }
