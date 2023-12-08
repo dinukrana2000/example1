3d6d63342906003dd4b91382f05848e3fc09b64a
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-08 15:33:09 +0530

    delete userdetails page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Home from './Pages/Home';
-import UserDetails from './Pages/UserDetails';
-//import PartOne from './Component/PartOne';
+
+
 
 function App() {
   return (
     <div className="App">
      
-     <UserDetails/>      
+    <Home/>    
     </div>
   );
 }
