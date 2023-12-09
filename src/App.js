f28921f9cdd489a1829d0fb903537bfea16d7fc8
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-09 17:51:35 +0530

    useState_Hook basics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import { useState } from 'react';
+
 
 
 
 function App() {
+  const[count,SetCount]=useState(0);
+  //var count=0;
+
+  const increment=()=>{
+    //count =count +1;
+    SetCount(count+1);
+
+  }
+
+  const decrement=()=>{
+    //count=count-1;
+    if(count>0){
+    SetCount(count-1);}
+  }
   return (
-    <div className="App">
-     
-    <Home/>    
-    </div>
+    <>
+    <span className='co'>My counter</span>
+    <p className='cal'>My count is {count}</p>
+    <button onClick={increment} className='plus'>+</button>
+    
+    <button onClick={decrement} className='minus'>-</button>
+    </>
   );
 }
 
