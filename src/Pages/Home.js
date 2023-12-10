4bae6a95d9885f3f86a6bbcc135a15cc70ad0fb5
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-10 19:01:15 +0530

    click event basics

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Topic from '../Component/Topic'
 import Content from '../Component/Content'
 
+function buttonClick(){
+  //onclick eke buttonClick function eka () ekka call krnne na***
+  console.log("button clicked");
+  alert("hey");
+}
+
 export default function Home() {
   
   return (
     <div>
-      <h1>Home page</h1>
-      <Topic/>
-      <Content/>
+    <button onClick={buttonClick}>Click me</button>
+      
+      
     </div>
   )
 }
