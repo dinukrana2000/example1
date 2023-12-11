038dd410d87f7550a21c72f286977900464c7df1
Dinuk Ranasinghe <144162881+dinukrana2000@users.noreply.github.com>
2023-12-11 11:54:03 +0530

    useEffect basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import { useState,useEffect } from 'react';
 
 
 
 function App() {
+  const [count,setCount]=useState(0);
+  const increment=()=>{
+    setCount(count + 1);
+
+  }
+
+  const decrement=()=>{
+    if(count>0){
+    setCount(count - 1)};
+  }
+
+  useEffect(()=>{
+
+    console.log('count is:'+count);
+   //alert("count is"+count);
+  },[count]
+  
+  )
   return (
     <div className="App">
-     
-    <Home/>    
+      <h1>Count:{count}</h1>
+       <button onClick={increment}>+</button>
+       
+       <button onClick={decrement}>-</button>
     </div>
   );
 }
